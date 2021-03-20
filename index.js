a4ca24a601b73088b19868d7de16a41bd405fa3a
tuanlong1202 <tuanlong1202@gmail.com>
2021-03-20 10:56:01 -0400

    Phase 0 intro to js 2 array lab

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Write your solution here!
+
+const cats = ["Milo","Otis", "Garfield"];
+
+function destructivelyAppendCat(name) {
+    cats.push(name);
+}
+
+function destructivelyPrependCat(name) {
+    cats.unshift(name);
+}
+
+function destructivelyRemoveLastCat(name) {
+    cats.pop(name);
+}
+
+function destructivelyRemoveFirstCat(name) {
+    cats.shift(name);
+}
+
+function appendCat(name) {
+    return [...cats,name];
+}
+
+function prependCat(name) {
+    return [name,...cats];
+}
+
+function removeFirstCat() {
+    return cats.slice(1,cats.length);
+}
+
+function removeLastCat() {
+    return cats.slice(0,cats.length-1);
+}
